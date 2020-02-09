5f1e515c9c40aaa6aea69f06bc74939f0eb5ca7b
Жигулин Николай <lublupelmeni@yandex.ru>
2020-02-09 09:24:24 +0000

    Upload New File

---
+++ 03_ObjectsAndClasses/3 module home/src/task3/zvezdochka/Cat.java
@@ -0,0 +1,72 @@
+package task3.zvezdochka;
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+    public boolean isAlive ;
+    public static int count;
+
+    private double minWeight;
+    private double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public void pee() {
+                   weight = weight - 250;
+          }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+
+
+    }
+
+    public void drink(Double amount) {
+            weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public  String getStatus() {
+        if (weight < minWeight)
+        {
+            return "Dead";
+        }
+        else if (weight > maxWeight)
+        {
+            return "Exploded";
+                }
+        else if (weight > originWeight) {
+            return "Sleeping";
+        } else {
+            return "Playing";
+        }
+    }
+
+    public  void catAlive()
+    {isAlive = true;
+        if (weight < minWeight)
+        {
+            isAlive = false;
+        }
+        else if (weight > maxWeight)
+        {
+            isAlive = false;
+        }
+
+    }
+}
+
+
