c76af93a7b5ac6b25c6b1c002e788d444c04e0f5
zhigulin_nikolai <lublupelmeni@yandex.ru>
2020-02-19 11:44:51 +0300

    4.1 home 2 version

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Container.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Container.java
@@ -1,4 +1,24 @@
 public class Container
 {
-    public Integer count;
+    public int count;
+    public Integer number;
+    public String stroka;
+
+    public  Integer sumDigits(Integer number)
+    {
+        this.number = number;
+        String digit;
+
+
+        digit = number.toString();
+        number = 0;
+        for (int i = 0; i < digit.length(); i++)
+        {
+            stroka = "" + digit.charAt(i); // Нашёл такую конструкцию чтоб
+            // вычленить каждую цифру и в следующей строке всё сложить
+            number = number + Integer.parseInt(stroka);
+        }
+        return number;
+    }
+
 }
--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -3,13 +3,17 @@ public class Main
     public static void main(String[] args)
     {
         Container container = new Container();
-        container.count += 7843;
+        container.count += 783;
+        System.out.println(container.count);
+
+
+        Container container2 = new Container();
+
+        System.out.println(" Сумма цифр = " + container2.sumDigits(container.count));
+
+
 
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
-    }
+
 }
