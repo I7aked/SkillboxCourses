d2ef86f0fcc75acf46d6511a8bb9084f646485a8
zhigulin_nikolai <lublupelmeni@yandex.ru>
2020-02-27 22:32:50 +0300

    4.2 и 4.3 home work

---
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/home_work_4_2/Main.java
@@ -0,0 +1,32 @@
+package home_work_4_2;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println(Byte.MAX_VALUE + " = Max byte");
+        System.out.println(Byte.MIN_VALUE + "= Min byte");
+        System.out.println();
+
+        System.out.println(Short.MAX_VALUE + "= Max short");
+        System.out.println(Short.MIN_VALUE + "= Min short");
+        System.out.println();
+
+        System.out.println(Integer.MAX_VALUE + "= Max integer");
+        System.out.println(Integer.MIN_VALUE + "= Min integer");
+        System.out.println();
+
+        System.out.println(Long.MAX_VALUE + "= Max long");
+        System.out.println(Long.MIN_VALUE + "= Min long");
+        System.out.println();
+
+        System.out.println(Double.MAX_VALUE + "= Max double");
+        System.out.println(Double.MIN_VALUE + "= Min double");
+        System.out.println();
+
+        System.out.println(Float.MAX_VALUE + "= Max float");
+        System.out.println(Float.MIN_VALUE + "= Min float");
+        System.out.println();
+
+
+    }
+}
