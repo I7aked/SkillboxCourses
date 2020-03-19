b56e42f4361d1a267f6a5f44880548b44bee933d
zhigulin_nikolai <lublupelmeni@yandex.ru>
2020-03-19 09:23:17 +0300

    4.6(2) Home work

--- 04_NumbersStringsAndDates/StringExperiments 4.6/src/DRDate.java
+++ 04_NumbersStringsAndDates/StringExperiments 4.6/src/DRDate.java
@@ -2,29 +2,29 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class DRDate {
 
     public static void main(String[] args) {
-       DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Date date = new Date(90,3,10);
+
+        Locale[] locales = {
+                Locale.getDefault(),
+                Locale.US};
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
 
         int myAge = 0;
         String birthDay="";
-       
+
         while (myAge<30) {
             date = calendar.getTime();
-            birthDay = date.toString();
-            birthDay = birthDay.replaceAll("([^A-z]{3,3}.*)$", "");
-            birthDay = birthDay.replaceAll("Apr", "");
+            System.out.println(myAge + " - " + dateFormat.format(date) + " - " + new SimpleDateFormat("EEEE",locales[0]).format(date));
+            System.out.println(myAge + " - " + dateFormat.format(date) + " - " + new SimpleDateFormat("E",locales[1]).format(date));
 
-//            System.out.println(calendar.getTime());
-//            System.out.println(birthDay);
-//            System.out.println(dateFormat.format(date));
-            System.out.println(myAge + " - " + dateFormat.format(date) + " - " + birthDay);
             calendar.add(calendar.MONTH,12);
             myAge++;
         }
