821598a5bc183891683f1a2b3eb7663be0a36a13
Fktrcfylh4 <Lublupelmeni@yandex.ru>
2020-04-03 14:12:39 +0430

    5.2(2) Home work

--- 05_ArraysAndCollections/5.2/src/Main.java
+++ 05_ArraysAndCollections/5.2/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int numberCommand =0;
         boolean ciklYslovie = true;
+        String vvodKonsol = "";
+        String mainCommand = "";
+
         Character elementListChange;
         ArrayList<String> commandList = new ArrayList<>(){{
             add("ADD");
@@ -35,32 +38,41 @@ public class Main {
             System.out.println(item);
         }
         while (ciklYslovie) {
-            System.out.println("Введите номер строки, если необходим, и через пробел команду для работы со списком");
+            System.out.println("Введите команду установленного образца");
 
             Scanner scanner = new Scanner(System.in);
             String commandListChange = scanner.nextLine();
-//
-            if (Character.isDigit(commandListChange.charAt(0)))
+
+            vvodKonsol=commandListChange.replaceAll("(([A-Z]+\\s)(\\d)?)",""); //Определяем вводимый продукт
+
+            mainCommand = commandListChange.replaceAll("(((\\d)?(\\s)+[A-Z][a-z]+).*)|(\\d?)","");//Определяем выполняемую команду
+
+            mainCommand = mainCommand.trim();
+            commandListChange = commandListChange.replaceAll("([A-Z]+\\s)?","");//Убирем команду из введённой строки
+
+
+            if (Character.isDigit(commandListChange.charAt(0)))// Проверяем есть ли номер строки в вводимой команде
             {
                 numberCommand = Integer.parseInt(commandListChange.substring(0, 1));//Записываем первую цифру переменную
-                commandListChange = commandListChange.replaceAll("[0-9]", "");//Стирем первую цифру с нашей переменной
-                commandListChange = commandListChange.trim();//Удаляем все незначащие пробелы
+//                commandListChange = commandListChange.replaceAll("[0-9]", "");//Стирем первую цифру с нашей переменной
+//                commandListChange = commandListChange.trim();//Удаляем все незначащие пробелы
             }
             else{
                 numberCommand = productList.size();
             }
 //
-            switch (commandListChange) {//определяем введённую команду и выбирем что делать
+            switch (mainCommand) {//определяем введённую команду и выбирем что делать
                 case "ADD":
-                    System.out.println("Введите новый продукт");
-                    productList.add(numberCommand,scanner.nextLine());
+                    System.out.println("Добавлен новый продукт");
+                    productList.add(numberCommand,vvodKonsol);
                     break;
                 case "EDIT":
-                    System.out.println("Введите необходимые изменения");
-                    productList.set(numberCommand, scanner.nextLine());
+                    System.out.println("Внесены необходимые изменения");
+                    productList.set(numberCommand, vvodKonsol);
                     break;
                 case "DELETE":
                     productList.remove(numberCommand);
+                    System.out.println("Выполнено");
                     break;
                 case "LIST":
                     for (String item: productList){
@@ -75,6 +87,7 @@ public class Main {
                 default:
                     System.out.println("Неправильная команда, перезапустите работу");
                     ciklYslovie = false;
+                    System.out.println(mainCommand);
                     break;
             }
             continue;
