ba16dd96336abf6a3f70b12dd74915443eb309a1
Fktrcfylh4 <Lublupelmeni@yandex.ru>
2020-05-26 15:52:44 +0430

    7.2 Home work

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -20,24 +20,47 @@ public class Main
     public static void main(String[] args)
     {
         ArrayList<Employee> staff = loadStaffFromFile();
+        Calendar calendar = Calendar.getInstance();
+        Date dateFirst = calendar.getTime();
+        calendar.setTimeInMillis(calendar.getTimeInMillis() + 720000);
+        Date dateSecond = calendar.getTime();
 
-        staff.stream()
-                .filter(e -> e.getWorkStart().before(new Date(01, 01, 2018)) && e.getWorkStart().after(new Date(31, 12,2016)))
+        calendar.set(2016, Calendar.DECEMBER,31);
+        long keyFirst = calendar.getTimeInMillis();
+        calendar.set(2018, Calendar.JANUARY,1);
+        long keySecond = calendar.getTimeInMillis();
+
+//        staff.stream()
+////                .filter(e -> e.getWorkStart().before(new Date(2018, Calendar.JANUARY, 1)))
+////                .filter(e -> e.getWorkStart().after(new Date(2016, Calendar.DECEMBER,31)))
+//                .max(Comparator.comparing(Employee::getSalary))
+//                .ifPresent(System.out::println);
+
+//        staff.stream()
+//                .filter(e -> e.getWorkStart().getTime() >= dateSecond.getTime() && e.getWorkStart().getTime() <= dateFirst.getTime())
+//                .max(Comparator.comparing(Employee::getSalary))
+//                .ifPresent(System.out::println);
+
+        staff.stream().
+                filter(e -> e.getWorkStart().getTime() > keyFirst)
+                .filter(e -> e.getWorkStart().getTime() < keySecond)
                 .max(Comparator.comparing(Employee::getSalary))
                 .ifPresent(System.out::println);
 
+
+
         Airport airport = Airport.getInstance();
+        airport.getTerminals().stream()//получаем список терминалов
+                .map(Terminal::getFlights)//получаем список полётов из этих терминалов
+                .flatMap(Collection::stream)//Сливаем все полёты в кучу
+                .filter(f -> f.getDate().after( new Date( System.currentTimeMillis())))
+                .filter(f -> f.getDate().before( new Date( System.currentTimeMillis() + 720000)))
+                .peek(System.out::print)//Выводим список времени полёта
+                .map(Flight::getAircraft)//переходим к моделям самолётов
+                .forEach(System.out::println);//выводим списки самолётов
 
-        airport.getTerminals().stream()
-                .map(Terminal::getFlights).filter();
 
 
-
-
-//        for (Employee employee: staff)
-//        {
-//            System.out.println(employee);
-//        }
     }
 
     private static ArrayList<Employee> loadStaffFromFile()
