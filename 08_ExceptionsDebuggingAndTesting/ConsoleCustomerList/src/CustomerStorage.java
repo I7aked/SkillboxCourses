e6a26e1daefa94985f32560c72b5cc0178107f4b
Fktrcfylh4 <Lublupelmeni@yandex.ru>
2020-05-29 16:48:51 +0430

    8.1 Home work

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CustomerStorage
 {
@@ -9,12 +10,56 @@ public class CustomerStorage
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
+//    public void addCustomer(String data)
+//    {
+//        try {
+//            String[] components = data.split("\\s+");
+//            String name = components[0] + " " + components[1];
+//            storage.put(name, new Customer(name, components[3], components[2]));
+//
+//             }catch (ArrayIndexOutOfBoundsException e) {
+//
+//            System.out.println("Вы ввели неверные данные для команды. Введите правильные");
+//            Scanner scanner = new Scanner(System.in);
+//            String command = scanner.nextLine();
+//            addCustomer(command);
+//        }
+//    }
+
+        public void addCustomer(String data)
     {
         String[] components = data.split("\\s+");
-        String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+        try {
+            try {
+                if (components.length != 4) {
+                    throw new Error();
+                }
+            } catch (Error e) {
+                System.out.print("Вы ввели не все данные для команды");
+                throw new Throwable();
+            }
+            String name = components[0] + " " + components[1];
+            try {
+                if (!components[2].matches(".+@.+\\..+")) throw new RuntimeException();
+            } catch (RuntimeException e) {
+                System.out.print("Вы ввели неправильную почту");
+                throw new Throwable();
+            }
+
+            try {
+                if (!components[3].matches("\\+?(\\d){11}")) throw new ArrayIndexOutOfBoundsException();
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.print("Вы ввели неверный номер телефона.");
+                throw new Throwable();
+            }
+            storage.put(name, new Customer(name, components[3], components[2]));
+        }catch (Throwable e){
+            System.out.println("Введите по образцу");
+        }
     }
+
+
 
     public void listCustomers()
     {
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -16,25 +16,25 @@ public class Main
         CustomerStorage executor = new CustomerStorage();
         for(;;)
         {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("list")) {
-                executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
-                executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
-                System.out.println(helpText);
-            }
-            else {
+            try {
+                    String command = scanner.nextLine();
+                    String[] tokens = command.split("\\s+", 2);
+
+                    if (tokens[0].equals("add")) {
+
+                        executor.addCustomer(tokens[1]);
+                    } else if (tokens[0].equals("list")) {
+                        executor.listCustomers();
+                    } else if (tokens[0].equals("remove")) {
+                        executor.removeCustomer(tokens[1]);
+                    } else if (tokens[0].equals("count")) {
+                        System.out.println("There are " + executor.getCount() + " customers");
+                    } else if (tokens[0].equals("help")) {
+                        System.out.println(helpText);
+                    }
+                    else {throw new Exception();}
+
+                } catch (Exception e){
                 System.out.println(commandError);
             }
         }
