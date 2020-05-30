3e751c340ba464fa85e1112c7fbbdd3c73b762ea
Fktrcfylh4 <Lublupelmeni@yandex.ru>
2020-05-30 19:25:46 +0430

    8.1(2) Home work

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -26,49 +26,62 @@ public class CustomerStorage
 //        }
 //    }
 
-        public void addCustomer(String data)
-    {
-        String[] components = data.split("\\s+");
-        try {
+        public void addCustomer(String data) {
+            String[] components = data.split("\\s+");
             try {
-                if (components.length != 4) {
-                    throw new Error();
+                if (storage.containsKey(components[0] + " " + components[1])) throw new Throwable();
+                try {
+                    try {
+                        if (components.length != 4) {
+                            throw new Error();
+                        }
+                    } catch (Error e) {
+                        System.out.print("Вы ввели не все данные для команды");
+                        throw new Exception();
+                    }
+                    String name = components[0] + " " + components[1];
+                    try {
+                        if (!components[2].matches(".+@.+\\..+")) throw new RuntimeException();
+                    } catch (RuntimeException e) {
+                        System.out.print("Вы ввели неправильную почту");
+                        throw new Exception();
+                    }
+
+                    try {
+                        if (!components[3].matches("\\+?(\\d){11}")) throw new ArrayIndexOutOfBoundsException();
+
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.print("Вы ввели неверный номер телефона.");
+                        throw new Exception();
+                    }
+                    storage.put(name, new Customer(name, components[3], components[2]));
+                } catch (Exception e) {
+                    System.out.println("Введите по образцу");
                 }
-            } catch (Error e) {
-                System.out.print("Вы ввели не все данные для команды");
-                throw new Throwable();
+            }catch (Throwable e)
+            {
+                System.out.println("Данный человек уже записан в базе");
             }
-            String name = components[0] + " " + components[1];
-            try {
-                if (!components[2].matches(".+@.+\\..+")) throw new RuntimeException();
-            } catch (RuntimeException e) {
-                System.out.print("Вы ввели неправильную почту");
-                throw new Throwable();
-            }
-
-            try {
-                if (!components[3].matches("\\+?(\\d){11}")) throw new ArrayIndexOutOfBoundsException();
-
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.print("Вы ввели неверный номер телефона.");
-                throw new Throwable();
-            }
-            storage.put(name, new Customer(name, components[3], components[2]));
-        }catch (Throwable e){
-            System.out.println("Введите по образцу");
         }
-    }
-
-
-
-    public void listCustomers()
-    {
-        storage.values().forEach(System.out::println);
+    public void listCustomers() {
+        try {
+            if (getCount() == 0) throw new ArrayIndexOutOfBoundsException();
+            storage.values().forEach(System.out::println);
+        }catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Ваш список пуст чтоб его выводить");
+        }
     }
 
     public void removeCustomer(String name)
     {
-        storage.remove(name);
+        try {
+            if (!storage.containsKey(name)) throw new NullPointerException();
+            storage.remove(name);
+        }  catch (NullPointerException e)
+        {
+            System.out.println("Данного человека нет в списке, введите по новой");
+        }
     }
 
     public int getCount()
