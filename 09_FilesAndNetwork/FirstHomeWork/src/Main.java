1475f56c8a8459355fa757dfbec3a9216be72c33
Fktrcfylh4 <Lublupelmeni@yandex.ru>
2020-08-12 14:08:40 +0430

    91.(2) Home work

--- 09_FilesAndNetwork/FirstHomeWork/src/Main.java
+++ 09_FilesAndNetwork/FirstHomeWork/src/Main.java
@@ -1,54 +1,88 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.LongStream;
 
 public class Main {
 
     private static long weightFiles;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите путь до папки");
-        String pathString = scanner.nextLine();
+        Path pathString = Paths.get(scanner.nextLine());
+
+//        try {
+//            File workDirectory = new File(pathString);
+//            weightFolder(workDirectory);
+//
+//        } catch (Exception e) {
+//            System.out.println("Вы указали неверный путь");
+//            e.printStackTrace();
+//        }
 
         try {
-            File workDirectory = new File(pathString);
-            weightFolder(workDirectory);
 
-        } catch (Exception e) {
+            Files.walk(pathString)
+                    .map(path -> new File(String.valueOf(path)))
+                    .filter(file -> !file.isDirectory())
+                    .mapToLong(File::length)
+                    .forEach(length -> weightFiles += length);
+
+        } catch (IOException e)
+        {
             System.out.println("Вы указали неверный путь");
             e.printStackTrace();
         }
+        weightAllFiles(weightFiles);
 
+
+//        if (weightFiles >= 107374182 ){
+//            System.out.println(weightFiles/107374182 +" GB");
+//        }
+//        else if (weightFiles >= 1048576) {
+//            System.out.println(weightFiles/1048576 +" MB");
+//        }
+//        else if (weightFiles >= 1024) {
+//            System.out.println(weightFiles/1024 +" kB");
+//        }
+//        else {   System.out.println(weightFiles +" Byt");
+//        }
+    }
+
+    public static void weightAllFiles (Long weightFiles)
+    {
         if (weightFiles >= 107374182 ){
-            System.out.println(weightFiles/107374182 +" GB");
+             System.out.println(weightFiles/107374182 + ","+ weightFiles % 107374182 +  " GB");
         }
         else if (weightFiles >= 1048576) {
-            System.out.println(weightFiles/1048576 +" MB");
+            System.out.println(weightFiles/1048576 + "," + weightFiles % 1048576 +" MB");
         }
         else if (weightFiles >= 1024) {
-            System.out.println(weightFiles/1024 +" kB");
+            System.out.println(weightFiles/1024 + "," + weightFiles % 1024 +" kB");
         }
         else {   System.out.println(weightFiles +" Byt");
         }
     }
 
-    public static void weightFolder(File foulder)
-    {
-        File[] folderEntries = foulder.listFiles();
-        for (File entry : folderEntries)
-        {
-            if (entry.isDirectory())
-            {
-                weightFiles = weightFiles + entry.length();
-                weightFolder(entry);
-                continue;
-            }
-            else weightFiles = weightFiles + foulder.length();
-        }
-
-    }
+//    public static void weightFolder(File foulder)
+//    {
+//        File[] folderEntries = foulder.listFiles();
+//        for (File entry : folderEntries)
+//        {
+//            if (entry.isDirectory())
+//            {
+//                weightFiles = weightFiles + entry.length();
+//                weightFolder(entry);
+//                continue;
+//            }
+//            else weightFiles = weightFiles + foulder.length();
+//        }
+//
+//    }
 }
